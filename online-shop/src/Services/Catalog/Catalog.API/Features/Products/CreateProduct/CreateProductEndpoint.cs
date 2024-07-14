53dad6bbf1b10c88460907dae301be744d22d392
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-14 09:49:03 +0330

    catalog - configuration in startup

--- online-shop/src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductEndpoint.cs
+++ online-shop/src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,26 @@
-﻿namespace Catalog.API.Features.Products.CreateProduct;
+﻿using Carter;
+using Catalog.API.Models.DTOs.Products.CreateProduct;
+using Mapster;
+using MediatR;
 
-public class CreateProductEndpoint
+namespace Catalog.API.Features.Products.CreateProduct;
+
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+    }
 }
--- online-shop/src/Services/Catalog/Catalog.API/Program.cs
+++ online-shop/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,15 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
+
+app.MapCarter();
+
 app.Run();
