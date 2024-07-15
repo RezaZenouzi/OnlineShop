5faad1d9ac562b7099f968e8ea5047fd4b97197b
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-15 17:34:24 +0330

    add marten 2

--- online-shop/src/Services/Catalog/Catalog.API/Program.cs
+++ online-shop/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,5 @@
 using Carter;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
