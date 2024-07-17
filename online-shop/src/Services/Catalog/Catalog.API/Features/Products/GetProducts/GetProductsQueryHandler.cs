1d4243cb7c80b979cb1e3184499dbf382a7b94e8
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-17 12:47:47 +0330

    complete get products

--- online-shop/src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsQueryHandler.cs
+++ online-shop/src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,11 +1,24 @@
 ﻿using BuildingBlocks.CQRS.Query;
 using Catalog.API.Models.DTOs.Products.GetProducts;
+using Catalog.API.Models.Entities;
+using Marten;
 
 namespace Catalog.API.Features.Products.GetProducts;
 public class GetProductsQueryHandler : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-    public Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    private readonly IDocumentSession _session;
+    private readonly ILogger<GetProductsQueryHandler> _logger;
+
+    public GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
     {
-        throw new NotImplementedException();
+        _session = session;
+        _logger = logger;
+    }
+
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("GetProductsQueryHandler.Handle called with {@query}", query);
+        var products = await _session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
     }
 }
---
+++ online-shop/src/Services/Catalog/Catalog.API/Models/DTOs/Products/GetProducts/GetProductsResponse.cs
@@ -0,0 +1,5 @@
+﻿using Catalog.API.Models.Entities;
+
+namespace Catalog.API.Models.DTOs.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
