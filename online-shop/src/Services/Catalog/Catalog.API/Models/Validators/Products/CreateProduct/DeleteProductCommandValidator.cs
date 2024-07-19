ec98bf5ddb2d85961e5fd686793686b56f6c77d6
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-19 13:05:45 +0330

    UpdateProductCommandValidator & DeleteProductCommandValidator

---
+++ online-shop/src/Services/Catalog/Catalog.API/Models/Validators/Products/CreateProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Models.DTOs.Products.DeleteProduct;
+using FluentValidation;
+
+namespace Catalog.API.Models.Validators.Products.CreateProduct;
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Product Id is required");
+    }
+}
--- online-shop/src/Services/Catalog/Catalog.API/Models/Validators/Products/CreateProduct/UpdateProductCommandValidator.cs
+++ online-shop/src/Services/Catalog/Catalog.API/Models/Validators/Products/CreateProduct/UpdateProductCommandValidator.cs
@@ -1,11 +1,14 @@
-﻿using Catalog.API.Models.DTOs.Products.CreateProduct;
+﻿using Catalog.API.Models.DTOs.Products.UpdateProduct;
 using FluentValidation;
 
 namespace Catalog.API.Models.Validators.Products.CreateProduct;
-public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
 {
-    public CreateProductCommandValidator()
+    public UpdateProductCommandValidator()
     {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Product Id is required");
         RuleFor(x => x.Name)
             .NotEmpty().WithMessage("Name is required")
             .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
