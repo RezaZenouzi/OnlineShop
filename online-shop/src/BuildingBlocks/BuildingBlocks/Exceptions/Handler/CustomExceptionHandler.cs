7b700db5b44c6180e92377db089a32734043405e
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-20 09:04:58 +0330

    create CustomExceptionHandler class

---
+++ online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Exceptions.Handler
+{
+    internal class CustomExceptionHandler
+    {
+    }
+}
