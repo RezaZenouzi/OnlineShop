34a22aa77c5abae7cbad5cb829313110d94b3433
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-20 09:13:31 +0330

    add AddExceptionHandler in program file

--- online-shop/src/Services/Catalog/Catalog.API/Program.cs
+++ online-shop/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,8 @@
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 using Carter;
 using FluentValidation;
 using Marten;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +19,7 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 builder.Services.AddCarter();
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 #endregion
 
@@ -28,30 +28,7 @@ var app = builder.Build();
 #region Configuration of HTTP Request Pipeline
 
 app.MapCarter();
-app.UseExceptionHandler(exceptionHandlerApp =>
-{
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if (exception is null)
-            return;
-
-        var problemDetails = new ProblemDetails()
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
+app.UseExceptionHandler(options => { });
 
 #endregion
 
