4e0fc245eaa1f4465427077911878f35d6516135
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-20 09:06:04 +0330

    install fluent validation

--- online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BuildingBlocks.Exceptions.Handler;
 
-namespace BuildingBlocks.Exceptions.Handler
+public class CustomExceptionHandler
 {
-    internal class CustomExceptionHandler
-    {
-    }
 }
