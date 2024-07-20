d8951464d172b7aecc49cec8a0344c3e113ee242
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-20 09:08:04 +0330

    complete CustomExceptionHandler

--- online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace BuildingBlocks.Exceptions.Handler;
@@ -15,6 +17,60 @@ public class CustomExceptionHandler : IExceptionHandler
 
     public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _logger.LogError(
+            "Error Message: {exceptionMessage}, Time of occurrence {time}",
+            exception.Message, DateTime.UtcNow);
+
+        (string Detail, string Title, int StatusCode) details = exception switch
+        {
+            InternalServerException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            ),
+            ValidationException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            BadRequestException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            NotFoundException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status404NotFound
+            ),
+            _ =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            )
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = details.Title,
+            Detail = details.Detail,
+            Status = details.StatusCode,
+            Instance = context.Request.Path
+        };
+
+        problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+
+        if (exception is ValidationException validationException)
+        {
+            problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
+        }
+
+        await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+        return true;
     }
 }
