6a5d834514e391825476535ccd836667d5958d87
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-20 09:07:22 +0330

    use ILogger in constructor

--- online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ online-shop/src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace BuildingBlocks.Exceptions.Handler;
 
 public class CustomExceptionHandler : IExceptionHandler
 {
-    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    private readonly ILogger<CustomExceptionHandler> _logger;
+
+    public CustomExceptionHandler(ILogger<CustomExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
