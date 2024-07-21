5cfecb336e9107a7d829a88e051329c6b3e02df8
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-21 11:00:02 +0330

    add ShoppingCart and ShoppingCartItem

---
+++ online-shop/src/Services/Basket/Basket.API/Models/Entities/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Models.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    //Required for Mapping
+    public ShoppingCart()
+    {
+    }
+
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
