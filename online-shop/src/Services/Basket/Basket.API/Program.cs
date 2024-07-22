d47ae84cc2349955aa66a0693f76ca41bf798303
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-22 08:33:31 +0330

    complete program file of basket api and add services

--- online-shop/src/Services/Basket/Basket.API/Program.cs
+++ online-shop/src/Services/Basket/Basket.API/Program.cs
@@ -1,12 +1,42 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using FluentValidation;
+using HealthChecks.UI.Client;
+using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services to Continer
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+builder.Services.AddCarter();
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 #endregion
 
 var app = builder.Build();
 
 #region Configuration of HTTP Request Pipeline
+
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 #endregion
 
