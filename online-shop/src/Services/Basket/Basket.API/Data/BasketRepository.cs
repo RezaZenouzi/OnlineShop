4c7934c170bbec6df54198fa862f990284791709
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-22 08:47:54 +0330

    start writting repository pattern for basket api with name of IBasketRepository

---
+++ online-shop/src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,29 @@
+﻿using Basket.API.Models.Entities;
+
+namespace Basket.API.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    #region Queries
+
+    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region Commands
+
+    public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
---
+++ online-shop/src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,19 @@
+﻿using Basket.API.Models.Entities;
+
+namespace Basket.API.Data;
+
+public interface IBasketRepository
+{
+    #region Queries
+
+    Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default);
+
+    #endregion
+
+    #region Commands
+
+    Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default);
+    Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
+
+    #endregion
+}
