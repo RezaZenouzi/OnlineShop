97d8a79065626e3a1d3c0d1ce59f14057ab70852
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-23 10:56:32 +0330

    complete CachedBasketRepository and install Microsoft.Extensions.Caching.StackExchangeRedis version 8.0.7

--- online-shop/src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
+++ online-shop/src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -1,21 +1,30 @@
 ﻿using Basket.API.Models.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace Basket.API.Data;
 
 public class CachedBasketRepository : IBasketRepository
 {
     private readonly IBasketRepository _repository;
+    private readonly IDistributedCache _cache;
 
-    public CachedBasketRepository(IBasketRepository repository)
+    public CachedBasketRepository(IBasketRepository repository, IDistributedCache cache)
     {
         _repository = repository;
+        _cache = cache;
     }
 
     #region Queries
 
     public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
     {
+        var cachedBasket = await _cache.GetStringAsync(userName, cancellationToken);
+        if (!string.IsNullOrEmpty(cachedBasket))
+            return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
         var basket = await _repository.GetBasket(userName, cancellationToken);
+        await _cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
         return basket;
     }
 
@@ -25,14 +34,16 @@ public class CachedBasketRepository : IBasketRepository
 
     public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
     {
-        var result = await _repository.StoreBasket(basket, cancellationToken);
-        return result;
+        await _repository.StoreBasket(basket, cancellationToken);
+        await _cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+        return basket;
     }
 
     public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
     {
-        var result = await _repository.DeleteBasket(userName, cancellationToken);
-        return result;
+        await _repository.DeleteBasket(userName, cancellationToken);
+        await _cache.RemoveAsync(userName, cancellationToken);
+        return true;
     }
 
     #endregion
