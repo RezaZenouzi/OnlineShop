ee682cad0440ab22a84a299d71fffc8a7070dd2a
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-23 10:40:04 +0330

    writting the base methods of CachedBasketRepository

---
+++ online-shop/src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Models.Entities;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository : IBasketRepository
+{
+    private readonly IBasketRepository _repository;
+
+    public CachedBasketRepository(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+
+    #region Queries
+
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var basket = await _repository.GetBasket(userName, cancellationToken);
+        return basket;
+    }
+
+    #endregion
+
+    #region Commands
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        var result = await _repository.StoreBasket(basket, cancellationToken);
+        return result;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var result = await _repository.DeleteBasket(userName, cancellationToken);
+        return result;
+    }
+
+    #endregion
+
+}
