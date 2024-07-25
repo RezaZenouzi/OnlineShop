a508638d0fabba0cece84243fb0c2243a3d39273
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-25 18:09:03 +0330

    add DiscountContext class

---
+++ online-shop/src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+}
