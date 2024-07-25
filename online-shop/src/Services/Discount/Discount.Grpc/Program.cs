0909a9dd8b06aaf97123670aced80f86faa700f6
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-25 10:03:18 +0330

    add MapGrpcService<DiscountService to program file

--- online-shop/src/Services/Discount/Discount.Grpc/Program.cs
+++ online-shop/src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,6 @@
 
+using Discount.Grpc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services to Continer
@@ -11,7 +13,7 @@ var app = builder.Build();
 
 #region Configuration of HTTP Request Pipeline
 
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 #endregion
