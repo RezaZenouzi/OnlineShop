f683f99820143c6d06479f2b4c980887fa719abe
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-25 09:53:46 +0330

    initial the second step of DiscountService

--- online-shop/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ online-shop/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,6 +1,34 @@
-﻿namespace Discount.Grpc.Services;
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services;
 
 public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
 {
+    #region Queries
 
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+
+    #endregion
+
+    #region Commands
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    #endregion
 }
