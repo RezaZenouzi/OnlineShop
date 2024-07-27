8851b07f1f1c7dcc8e6943f174c46a07721f9560
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-27 13:56:55 +0330

    add extension for add services to continer

--- online-shop/src/Services/Ordering/Ordering.API/Program.cs
+++ online-shop/src/Services/Ordering/Ordering.API/Program.cs
@@ -1,8 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services to Continer
 
-
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddOrderingApiServices();
 
 #endregion
 
