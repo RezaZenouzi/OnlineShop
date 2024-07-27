be10a47210751f795e342572cdb321f6a5001f16
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-27 10:37:46 +0330

    add ordering api project

---
+++ online-shop/src/Services/Ordering/Ordering/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Services to Continer
+
+
+
+#endregion
+
+var app = builder.Build();
+
+#region Configuration of HTTP Request Pipeline
+
+
+
+#endregion
+
+app.Run();
