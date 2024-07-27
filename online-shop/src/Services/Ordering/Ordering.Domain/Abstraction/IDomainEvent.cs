40d64edb9ad571c79ad6ae50fb15d410d94f130a
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-27 14:25:15 +0330

    create IDomainEvent

---
+++ online-shop/src/Services/Ordering/Ordering.Domain/Abstraction/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstraction;
+
+public interface IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
