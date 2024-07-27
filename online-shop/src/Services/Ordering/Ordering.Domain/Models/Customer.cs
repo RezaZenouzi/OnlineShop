8eaeee63bb5fcc6d78589ec6ec6e00fc6c326805
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-27 14:45:27 +0330

    add models and enums and value objects for ordering microservice

---
+++ online-shop/src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using Ordering.Domain.Abstraction;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models;
+
+public class Customer : Entity<CustomerId>
+{
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+
+    public static Customer Create(CustomerId id, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        var customer = new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+
+        return customer;
+    }
+}
---
+++ online-shop/src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using Ordering.Domain.Abstraction;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<OrderItemId>
+{
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+    {
+        Id = OrderItemId.Of(Guid.NewGuid());
+        OrderId = orderId;
+        ProductId = productId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public OrderId OrderId { get; private set; } = default!;
+    public ProductId ProductId { get; private set; } = default!;
+    public int Quantity { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+}
