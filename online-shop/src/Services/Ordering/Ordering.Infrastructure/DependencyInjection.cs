258d9fa2eba56839d7b43ab03f8c39eb6fbf2e45
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-29 09:38:09 +0330

    configure db context in dependency injection

--- online-shop/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ online-shop/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -8,6 +11,19 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
+
+        // Add services to the container.
+        //services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        //services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
         return services;
     }
 }
