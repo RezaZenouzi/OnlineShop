86b0b634eeb308190656699793c5c0c0018b4b3e
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-07-30 08:49:37 +0330

    seeding data

--- online-shop/src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ online-shop/src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -11,5 +11,41 @@ public static class DatabaseExtensions
         await using var scope = app.Services.CreateAsyncScope();
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomerAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrdersWithItemsAsync(context);
+    }
+
+    private static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrdersWithItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync();
+        }
     }
 }
