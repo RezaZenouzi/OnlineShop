4abfd96d745916e9866be54f84d01e66977e5ba1
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-01 09:39:46 +0330

    complete 50 percent of CreateOrderCommandHandler

--- online-shop/src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,5 +1,12 @@
-﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+﻿using BuildingBlocks.CQRS.Command;
+using Ordering.Application.Models.DTOs.CreateOrder;
 
-public class CreateOrderCommandHandler
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
