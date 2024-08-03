e414616b1a2337393bf1650226360028d288c312
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-03 10:20:26 +0330

    add OrderUpdatedEventHandler and OrderCreatedEventHandler and complete them for first step

---
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
+{
+    private readonly ILogger<OrderCreatedEventHandler> _logger;
+
+    public OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
---
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderUpdatedEventHandler : INotificationHandler<OrderUpdatedEvent>
+{
+    private readonly ILogger<OrderCreatedEventHandler> _logger;
+
+    public OrderUpdatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
