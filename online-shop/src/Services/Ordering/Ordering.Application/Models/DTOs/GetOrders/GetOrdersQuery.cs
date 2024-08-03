e9a10b17db77cf5afc61ab4fbc078e89f8fc7769
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-03 10:41:22 +0330

    complete GetOrdersQueryHandler

---
+++ online-shop/src/Services/Ordering/Ordering.Application/Models/DTOs/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.CQRS.Query;
+using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Models.DTOs.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
---
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using BuildingBlocks.CQRS.Query;
+using BuildingBlocks.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Extensions;
+using Ordering.Application.Models.DTOs._Public;
+using Ordering.Application.Models.DTOs.GetOrders;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+public class GetOrdersQueryHandler : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+
+    public GetOrdersQueryHandler(IApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var totalCount = await _applicationDbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await _applicationDbContext.Orders
+            .Include(o => o.OrderItems)
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(
+            new PaginatedResult<OrderDto>(
+                pageIndex,
+                pageSize,
+                totalCount,
+                orders.ToOrderDtoList()));
+    }
+}
