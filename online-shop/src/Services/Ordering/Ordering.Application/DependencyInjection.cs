bd741ec5c0b2263f591762b3e5077a47201576a8
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-03 10:06:05 +0330

    add ValidationBehavior and LoggingBehavior for mediatr application order

--- online-shop/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ online-shop/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Ordering.Application;
@@ -10,6 +11,8 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
         return services;
