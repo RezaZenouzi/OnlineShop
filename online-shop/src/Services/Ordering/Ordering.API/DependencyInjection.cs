dda85e2daee45fb09c33ff2cb25c12c6d63e768d
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-04 09:23:11 +0330

    add carter for ordering api

--- online-shop/src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ online-shop/src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,18 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddOrderingApiServices(this IServiceCollection services)
     {
+        services.AddCarter();
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.MapCarter();
         return app;
     }
 }
