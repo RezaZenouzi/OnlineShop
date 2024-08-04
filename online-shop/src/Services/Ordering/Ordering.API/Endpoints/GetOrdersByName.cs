25cb459a36c5d6309f1e78729d46dde1346854c7
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-04 09:52:39 +0330

    add and complete GetOrdersByName endpoint

--- online-shop/src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ online-shop/src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,5 +1,24 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Models.DTOs.GetOrdersByName;
 
-public class GetOrdersByName
+namespace Ordering.API.Endpoints;
+
+public class GetOrdersByName : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+                var response = result.Adapt<GetOrdersByNameResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByName")
+            .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Orders By Name")
+            .WithDescription("Get Orders By Name");
+    }
 }
---
+++ online-shop/src/Services/Ordering/Ordering.Application/Models/DTOs/GetOrdersByName/GetOrdersByNameRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Ordering.Application.Models.DTOs.GetOrdersByName;
+
+public record GetOrdersByNameRequest(string Name);
