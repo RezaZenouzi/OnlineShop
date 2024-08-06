b9d4be359b5b9398638a0b64232c64b8c5de98ae
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-06 09:06:52 +0330

    create BasketCheckoutEvent and IntegrationEvent classes

---
+++ online-shop/src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
