1556140fb5dc7256ff2301b8b5d17c0e63cada7b
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-06 09:21:16 +0330

    complete AddMessageBroker method

--- online-shop/src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
+++ online-shop/src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -1,5 +1,32 @@
-﻿namespace BuildingBlocks.Messaging.MassTransit;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
-internal class Extentions
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extentions
 {
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]);
+                    host.Password(configuration["MessageBroker:Password"]);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
 }
