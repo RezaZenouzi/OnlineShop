65dfdb01ed9dea8b181ee8dd660928529eb4eba6
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-10 09:09:26 +0330

    create BasketCheckoutEventHandler integration

---
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration;
+
+public class BasketCheckoutEventHandler : IConsumer<BasketCheckoutEvent>
+{
+    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        throw new NotImplementedException();
+    }
+}
