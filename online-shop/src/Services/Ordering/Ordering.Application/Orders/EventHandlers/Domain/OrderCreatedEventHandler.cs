0ade0cc6a2bfec8017fd578ef3529c4c36d7037f
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-10 09:33:58 +0330

    complete OrderCreatedEventHandler

--- online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using MassTransit;
+using MediatR;
 using Microsoft.Extensions.Logging;
+using Ordering.Application.Extensions;
 using Ordering.Domain.Events;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
@@ -7,15 +9,19 @@ namespace Ordering.Application.Orders.EventHandlers.Domain;
 public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
 {
     private readonly ILogger<OrderCreatedEventHandler> _logger;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    public OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger, IPublishEndpoint publishEndpoint)
     {
         _logger = logger;
+        _publishEndpoint = publishEndpoint;
     }
 
-    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
-        return Task.CompletedTask;
+
+        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+        await _publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
     }
 }
