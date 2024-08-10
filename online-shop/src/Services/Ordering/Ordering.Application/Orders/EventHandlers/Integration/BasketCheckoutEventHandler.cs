0ea1688b61b2b199317a7db6dbd70ee67ea74054
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-10 09:22:15 +0330

    complete BasketCheckoutEventHandler

--- online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -1,12 +1,52 @@
 ﻿using BuildingBlocks.Messaging.Events;
 using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Models.DTOs._Public;
+using Ordering.Application.Models.DTOs.CreateOrder;
 
 namespace Ordering.Application.Orders.EventHandlers.Integration;
 
 public class BasketCheckoutEventHandler : IConsumer<BasketCheckoutEvent>
 {
-    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    private readonly ISender _sender;
+    private readonly ILogger<BasketCheckoutEventHandler> _logger;
+
+    public BasketCheckoutEventHandler(ISender sender, ILogger<BasketCheckoutEventHandler> logger)
     {
-        throw new NotImplementedException();
+        _sender = sender;
+        _logger = logger;
+    }
+
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        _logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message, GetType().Name);
+
+        var command = MapToCreateOrderCommand(context.Message);
+        await _sender.Send(command);
+    }
+
+    private CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+    {
+        // Create full order with incoming event data
+        var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+        var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.CVV, message.PaymentMethod);
+        var orderId = Guid.NewGuid();
+
+        var orderDto = new OrderDto(
+            Id: orderId,
+            CustomerId: message.CustomerId,
+            OrderName: message.UserName,
+            ShippingAddress: addressDto,
+            BillingAddress: addressDto,
+            Payment: paymentDto,
+            Status: Ordering.Domain.Enums.OrderStatus.Pending,
+            OrderItems: new List<OrderItemDto>()
+            {
+                new(orderId, new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"), 2, 500),
+                new(orderId, new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914"), 1, 400)
+            });
+
+        return new CreateOrderCommand(orderDto);
     }
 }
