6827ee32e18af36e9a37e8481662cd69c9c6c7c9
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-10 10:11:52 +0330

    add OrderFullfilment for flag and seting it up in OrderCreatedEventHandler

--- online-shop/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ online-shop/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 using System.Reflection;
 
 namespace Ordering.Application;
@@ -17,6 +18,7 @@ public static class DependencyInjection
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
+        services.AddFeatureManagement();
         services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
         return services;
--- online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ online-shop/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MassTransit;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 using Ordering.Application.Extensions;
 using Ordering.Domain.Events;
 
@@ -10,18 +11,26 @@ public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
 {
     private readonly ILogger<OrderCreatedEventHandler> _logger;
     private readonly IPublishEndpoint _publishEndpoint;
+    private readonly IFeatureManager _featureManager;
 
-    public OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger, IPublishEndpoint publishEndpoint)
+    public OrderCreatedEventHandler(
+        ILogger<OrderCreatedEventHandler> logger,
+        IPublishEndpoint publishEndpoint,
+        IFeatureManager featureManager)
     {
         _logger = logger;
         _publishEndpoint = publishEndpoint;
+        _featureManager = featureManager;
     }
 
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-        await _publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        if (await _featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+            await _publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
