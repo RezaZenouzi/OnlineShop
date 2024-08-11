40c838e5fbda2dabebc26511daaeebc33b3b02c9
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-11 16:59:02 +0330

    some set up for yarp api gateway

--- online-shop/src/ApiGateways/YarpApiGateway/Program.cs
+++ online-shop/src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+#region Services to Continer
+
+
+
+#endregion
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region Configuration of HTTP Request Pipeline
+
+
+
+#endregion
 
 app.Run();
