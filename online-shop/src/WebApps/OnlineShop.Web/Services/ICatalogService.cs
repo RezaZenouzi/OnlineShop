da34a080052535fc4b2fc5fb7cee5ccc2a6b9258
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-11 20:59:28 +0330

    creating ICatalogService

---
+++ online-shop/src/WebApps/OnlineShop.Web/Services/ICatalogService.cs
@@ -0,0 +1,15 @@
+﻿using OnlineShop.Web.Models.Catalog;
+
+namespace OnlineShop.Web.Services;
+
+public interface ICatalogService
+{
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
+    Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+
+    [Get("/catalog-service/products/{id}")]
+    Task<GetProductByIdResponse> GetProduct(Guid id);
+
+    [Get("/catalog-service/products/category/{category}")]
+    Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+}
