18fba38cbd0b4f0d36a2a116a89986dc7e354d2f
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-11 17:20:00 +0330

    config urls in yarp and config RateLimiter

--- online-shop/src/ApiGateways/YarpApiGateway/Program.cs
+++ online-shop/src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services to Continer
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(rateLimitedOptions =>
+{
+    rateLimitedOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 
 #endregion
 
@@ -10,6 +20,7 @@ var app = builder.Build();
 
 #region Configuration of HTTP Request Pipeline
 
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 #endregion
