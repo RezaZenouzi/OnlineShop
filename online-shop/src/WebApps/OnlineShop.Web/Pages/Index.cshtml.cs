3e13a1e72a4a70440d9530543382f1b481d1487e
Reza Zenouzi <ahmadrezazenouzi@gmail.com>
2024-08-11 21:20:08 +0330

    complete index page

--- online-shop/src/WebApps/OnlineShop.Web/Pages/Index.cshtml.cs
+++ online-shop/src/WebApps/OnlineShop.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineShop.Web.Models.Basket;
+using OnlineShop.Web.Models.Catalog;
+using OnlineShop.Web.Services;
 
-namespace OnlineShop.Web.Pages
+namespace OnlineShop.Web.Pages;
+public class IndexModel
+    : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ICatalogService _catalogService;
+    private readonly IBasketService _basketService;
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ICatalogService catalogService, IBasketService basketService, ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _catalogService = catalogService;
+        _basketService = basketService;
+        _logger = logger;
+    }
+    public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
 
-        public IndexModel(ILogger<IndexModel> logger)
+    public async Task<IActionResult> OnGetAsync()
+    {
+        _logger.LogInformation("Index page visited");
+        var result = await _catalogService.GetProducts();
+        ProductList = result.Products;
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        _logger.LogInformation("Add to cart button clicked");
+
+        var productResponse = await _catalogService.GetProduct(productId);
+
+        var basket = await _basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
         {
-            _logger = logger;
-        }
+            ProductId = productId,
+            ProductName = productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
 
-        public void OnGet()
-        {
+        await _basketService.StoreBasket(new StoreBasketRequest(basket));
 
-        }
+        return RedirectToPage("Cart");
     }
 }
